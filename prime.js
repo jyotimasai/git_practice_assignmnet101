b2709d49c2759b3e30cae75da792e6f6b038768a
jyotimasai <gsjyotimeena0@gmail.com>
2022-12-08 20:16:29 +0530

    task101

---
+++ prime.js
@@ -0,0 +1,30 @@
+ 
+prime_problem_js101_
+ 
+ let num =13;
+  
+ let count =0;
+ 
+ for(let i=0;i<=num;i++){
+     
+ if(num%i==0){
+      
+    count++;
+              
+}
+
+
+ }
+    
+
+if(count ==2){
+    
+console.log('its  not a prime number');
+   
+ }
+   else{
+    
+console.log('its a priem number');
+   
+   }
+ 
